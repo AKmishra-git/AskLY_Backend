53a72423e8aefcdb42851af94c7cf85460071744
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-05-03 17:59:19 +0530

    sameSite fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -147,7 +147,7 @@ export async function loginController(req, res) {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
