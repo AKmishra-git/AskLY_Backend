8a87674e0f29485c3fe76aad3c42ff8e2890dea3
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-05-03 17:45:44 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Update this to your frontend URL
+    origin: ['https://askly-dev.netlify.app', 'http://localhost:5173'],
     credentials: true   
 }));
 app.use(express.json());
